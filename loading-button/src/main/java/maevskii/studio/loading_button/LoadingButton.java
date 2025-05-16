6d05f59b56293bcf54aaa185a1a24bd62aa3505c
Mikhail Maevskiy <57516381+memaev@users.noreply.github.com>
2025-05-17 01:05:21 +0300

    Added functionality for changing button size

--- loading-button/src/main/java/maevskii/studio/loading_button/LoadingButton.java
+++ loading-button/src/main/java/maevskii/studio/loading_button/LoadingButton.java
@@ -1,11 +1,13 @@
 package maevskii.studio.loading_button;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -13,6 +15,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 public class LoadingButton extends LinearLayout {
 
@@ -51,6 +54,7 @@ public class LoadingButton extends LinearLayout {
         setEnabled(true);
         buttonTextView.setEnabled(true);
         buttonTextView.setTextColor(arr.getColor(R.styleable.LoadingButton_android_textColor, Color.BLACK));
+
         int globalPadding = arr.getInteger(R.styleable.LoadingButton_android_padding, 0);
         this.setPadding(
                 Math.max(globalPadding, arr.getInteger(R.styleable.LoadingButton_android_paddingStart, 0)),
@@ -59,6 +63,8 @@ public class LoadingButton extends LinearLayout {
                 Math.max(globalPadding, arr.getInteger(R.styleable.LoadingButton_android_paddingBottom, 0))
         );
         setText(arr.getString(R.styleable.LoadingButton_android_text));
+        setBackgroundResource(R.drawable.button_background);
+        setGravity(Gravity.CENTER);
 
         setLoading(arr.getBoolean(R.styleable.LoadingButton_isLoading, false));
         progressBar.getIndeterminateDrawable().setColorFilter(arr.getColor(R.styleable.LoadingButton_progressBarColor, Color.BLACK), PorterDuff.Mode.SRC_IN);
